72db43281f2cce6cf39948100a60bb495ef1c140
Alvaro Lechuga <alvarolechugahierro@gmail.com>
2021-12-11 21:29:55 +0100

    Dinamizado temporizador

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,76 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public hora: string = '00:00:00';
+  public horas: number = 0;
+  public minutos: number = 0;
+  public segundos: number = 0;
+  
+  public horasString: string = '00';
+  public minutosString: string = '00';
+  public segundosString: string = '00';
 
+  public iniciar: boolean = false;
+  public comienzo: boolean = true;
+
+  public intervalo: any;
+
+  constructor(
+  ) {}
+
+  public ngOnDestroy() {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
+    }
+  }
+
+  public empezar(): void {
+    this.comienzo = false;
+    this.iniciar = true;
+
+    const time = this.hora.split(':');
+    this.horas = +time[0];
+    this.minutos = +time[1];
+    this.segundos = +time[2];
+
+    this.horasString = this.minTwoDigits(+time[0]);
+    this.minutosString = this.minTwoDigits(+time[1]);
+    this.segundosString = this.minTwoDigits(+time[2]);
+
+    this.cargar();
+  }
+
+  public reiniciar(): void {
+    this.comienzo = true;
+    this.iniciar = false;
+    this.hora = '00:00:00';
+  }
+
+  public cargar(): void {
+    this.intervalo = setInterval(() => {
+      if (this.iniciar) {
+        if (this.segundos === 60) {
+          this.segundos = 0;
+          this.segundosString = '00';
+          this.minutos--;
+          this.minutosString = this.minTwoDigits(this.minutos);
+          if (this.minutos === 60) {
+            this.minutos = 0;
+            this.minutosString = '00';
+            this.horas--;
+            this.horasString = this.minTwoDigits(this.horas);
+          }
+        }
+        this.segundos--;
+        this.segundosString = this.minTwoDigits(this.segundos);
+      }
+      if (this.segundos == 0 && this.minutos === 0 && this.horas === 0) {
+        this.ngOnDestroy();
+      }
+    }, 1000);
+  }
+
+  private minTwoDigits(n): string {
+    return (n < 10 ? '0' : '') + n;
+  }
 }
