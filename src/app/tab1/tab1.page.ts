54f3e431ecb5b0ad394071ba4d65d324b54b3c17
Alvaro Lechuga <alvarolechugahierro@gmail.com>
2021-12-11 17:10:47 +0100

    Dinamizado modo cronometro

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,81 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public horas: number = 0;
+  public minutos: number = 0;
+  public segundos: number = 0;
+  
+  public horasString: string = '00';
+  public minutosString: string = '00';
+  public segundosString: string = '00';
+
+  public iniciar: boolean = false;
+
+  public intervalo: any;
+
+  constructor(
+    private _alertCtrl: AlertController
+  ) { }
+
+  public ngOnDestroy() {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
+    }
+  }
+
+  public start(): void {
+    this.iniciar = !this.iniciar;
+    if (this.iniciar && !this.intervalo) {
+      this.cargar();
+    } 
+  }
+
+  public reiniciar(): void {
+    this._alertCtrl.create({
+      header: 'Reiniciar cronómetro',
+      message: '¿Quires reiniciar el cronómetro?',
+      buttons: [
+        {
+          text: 'Cancelar'
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.horas = 0;
+            this.minutos = 0;
+            this.segundos = 0;
+            this.iniciar = false;
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    });
+  }
+
+  public cargar(): void {
+    this.intervalo = setInterval(() => {
+      if (this.iniciar) {
+        if (this.segundos === 60) {
+          this.segundos = 0;
+          this.segundosString = '00';
+          this.minutos++;
+          this.minutosString = this.minTwoDigits(this.minutos);
+          if (this.minutos === 60) {
+            this.minutos = 0;
+            this.minutosString = '00';
+            this.horas++;
+            this.horasString = this.minTwoDigits(this.horas);
+          }
+        }
+        this.segundos++;
+        this.segundosString = this.minTwoDigits(this.segundos);
+      }
+    }, 1000);
+  }
+
+  private minTwoDigits(n): string {
+    return (n < 10 ? '0' : '') + n;
+  }
 
 }
